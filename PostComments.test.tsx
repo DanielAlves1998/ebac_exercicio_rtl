c9667c6a3e15384b04581df4bcdbc8b341cc6746
Daniel Alves <daniel199831.da@gmail.com>
2025-04-28 20:55:52 -0300

    alteração

---
+++ PostComments.test.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import { fireEvent, render, screen } from '@testing-library/react';
+import PostComment from '.';
+
+describe('Teste para o componente PostComment', () => {
+    it('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>);
+        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment/>);
+
+
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+    
+        fireEvent.change(screen.getByTestId('comment-textarea'), {
+            target: {
+                value: 'Segundo comentário adicionado via testes',
+            }
+        });
+        fireEvent.click(screen.getByTestId('comment-button'));
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
+    });
+});
